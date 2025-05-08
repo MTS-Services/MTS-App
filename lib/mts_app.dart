31e61b40c25f63d7b58fca295751140f9c77d2b9
remon <remonhowlader97@gmail.com>
2025-05-08 09:17:45 +0600

    add dark mood

--- lib/mts_app.dart
+++ lib/mts_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mts_app/src/view/screen/home_screen.dart';
-import 'package:mts_app/src/view/screen/login_screen.dart';
+
 
 class MtsApp extends StatelessWidget {
   const MtsApp({super.key});
@@ -10,6 +10,12 @@ class MtsApp extends StatelessWidget {
     return MaterialApp(
       title: 'MTS App',
       home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+      ),
     );
   }
 }
