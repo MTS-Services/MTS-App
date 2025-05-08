0e550736a0405aeefb1501d6c310084d986fad1a
mdtayobali13 <mdtayobalideveloper@gmail.com>
2025-05-08 10:09:15 +0600

    colors add

---
+++ lib/core/color_path.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColor{
+  static const Color amountText = Color(0xffFFFFFF);
+  static const Color 	statusBadge = Color(0xffFFC107);
+  static const Color 	dateBackground = Color(0xff00C851);
+  static const Color 	statusText = Color(0xffF44336);
+  static const Color 	salesComment = Color(0xff11284A);
+  static const Color 	background = Color(0xff192034);
+
+}
